b2e5af2bda3cb7ff79c7615e01f140daa8efdc31
Marc D <marcd@break-first.com>
2026-05-01 19:45:31 +0200

    (cs) FilmUser : renamed field Seen to Watched

--- Watchlist/Data/FilmUser.cs
+++ Watchlist/Data/FilmUser.cs
@@ -4,7 +4,7 @@
     {
         public int UserID {  get; set; }
         public int FilmID { get; set; }
-        public bool Seen { get; set; }
+        public bool Watched { get; set; }
         public int Note { get; set; }
         public virtual AccountUser User { get; set; }
         public virtual Film Film { get; set; }
