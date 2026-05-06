95c4bdf02233ffe0ecef35f2de3005f8042cff81
Marc D <marcd@break-first.com>
2026-05-06 17:42:34 +0200

    (cs, cshtml) Film User List : implemented details and edit methods and views

--- Watchlist/Controllers/FilmListController.cs
+++ Watchlist/Controllers/FilmListController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Watchlist.Data;
 using Watchlist.Models;
+
 
 namespace Watchlist.Controllers
 {
@@ -40,6 +42,90 @@ namespace Watchlist.Controllers
             }).ToList();
 
             return View(model);
+        }
+
+        // GET: FilmList/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            var userId = await GetCurrentUserId();
+
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var film = await m_context.FilmUsers.FirstOrDefaultAsync(x => x.UserId == userId && x.FilmID == id);
+            if (film == null)
+            {
+                return NotFound();
+            }
+
+            return View(film);
+        }
+
+        // GET: FilmList/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            var userId = await GetCurrentUserId();
+
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var film = await m_context.FilmUsers.FirstOrDefaultAsync(x => x.UserId == userId && x.FilmID == id);
+            if (film == null)
+            {
+                return NotFound();
+            }
+            return View(film);
+        }
+
+        // POST: FilmList/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("UserId,AccountUser,FilmID,Watched,Note")] FilmUser filmUser)
+        {
+            var userId = await GetCurrentUserId();
+
+            if (!FilmUserExists(filmUser.FilmID, filmUser.UserId))
+            {
+                return NotFound();
+            }
+            if (filmUser.UserId != userId)
+            {
+                return NotFound();
+            }
+            ModelState.Remove("AccountUser");
+            ModelState.Remove("Film");
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    m_context.Update(filmUser);
+                    await m_context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!FilmUserExists(filmUser.FilmID, filmUser.UserId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(filmUser);
+        }
+
+        private bool FilmUserExists(int filmID, string userId)
+        {
+            return m_context.FilmUsers.Any(e => e.FilmID == filmID && e.UserId == userId);
         }
     }
 }
